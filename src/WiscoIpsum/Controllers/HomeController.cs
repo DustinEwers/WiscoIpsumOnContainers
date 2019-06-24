033311e6ae43652aa738f7adb7adc8ba4a80964c
Dustin Ewers <dustin.ewers@gmail.com>
2019-06-23 23:44:03 -0500

    initial app

---
+++ src/WiscoIpsum/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WiscoIpsum.Models;
+using WiscoIpsum.Services;
+
+namespace WiscoIpsum.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new IpsumViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(IpsumViewModel model)
+        {
+            model.IpsumText = IpsumGenerator.GenerateIpsum(model.Paragraphs);
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/WiscoIpsum/Services/IpsumGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+
+namespace WiscoIpsum.Services
+{
+    public class IpsumGenerator
+    {
+        public static string GenerateIpsum(int numberOfParagraphs) {
+            if (numberOfParagraphs < 1) { numberOfParagraphs = 1; }
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < numberOfParagraphs; i++) {
+                sb.AppendLine(GenerateParagraph());
+                if (i + 1 < numberOfParagraphs) {
+                    sb.AppendLine(Environment.NewLine);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private static string GenerateParagraph()
+        {
+            var random = new Random();
+            var phases = GetPhrases();
+            var numberOfphrases = random.Next(10, 30);
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < numberOfphrases; i++) {
+                var index = random.Next(0, phases.Length - 1);
+                sb.Append(phases[index]);
+                sb.Append(" ");
+            }
+
+            return sb.ToString();
+        }
+
+        private static string[] GetPhrases() => new string[] {
+            "Ope",
+            "Where-Abouts",
+            "Spotted Cow",
+            "Brandy Old Fashioned",
+            "Stop-and-go-lights",
+            "Fleet Farm",
+            "Cheeshead",
+            "Fish Fry",
+            "Bubbler",
+            "Aw Geez",
+            "For Cripes Sakes",
+            "Up Nort",
+            "Uff-Da",
+            "Ya Know?",
+            "Believe You Me",
+            "You betcha"
+        };
+    }
+}
